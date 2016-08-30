898ef12de54ba8dc1c87b2f553d2fbecd17c0492
Danny Dutton <duttondj@vt.edu>
2016-08-30 02:15:37 -0400

    Bug fixes for screenswitch.sh

--- screenswitch.sh
+++ screenswitch.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
-xrandr --output VGA1 --off --output LVDS1 --auto
+#Look for VGA or displayport connected and being used.
+#Assumes that only one of the connections is actually being used.
+screenmode="$(xrandr| grep -E 'VGA.\ connected [0-9]|DP.\ connected [0-9]' | awk '/1/ {print $1}')"
 
-i3-nagbar -m "SWITCH MONITORS" -t warning \
-	-b "LAPTOP"  "xrandr --output VGA1 --off --output LVDS1 --auto" \
-	-b "MONITOR"  "xrandr --output LVDS1 --off --output VGA1 --auto"
+#Switch to laptop screen if VGA or DP is being used
+#Switch to VGA and DP if laptop is being used
+case "$screenmode" in
+	VGA1)
+		xrandr --output LVDS1 --auto --output DP1 --off --output VGA1 --off
+	;;
+	DP1)	
+		xrandr --output LVDS1 --auto --output DP1 --off --output VGA1 --off
+	;;
+	*)
+		xrandr --output LVDS1 --off --output DP1 --auto --output VGA1 --auto
+	;;
+esac
