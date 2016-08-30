c7c898ec3a73b10b9ebd6c693c9f0dd9fcdba123
Danny Dutton <duttondj@vt.edu>
2016-08-30 06:09:55 -0400

    refactoring of screenswitch, works much clearer now

--- screenswitch.sh
+++ screenswitch.sh
@@ -2,18 +2,18 @@
 
 #Look for VGA or displayport connected and being used.
 #Assumes that only one of the connections is actually being used.
-screenmode="$(xrandr| grep -E 'VGA.\ connected [0-9]|DP.\ connected [0-9]' | awk '/1/ {print $1}')"
+screenmode="$(xrandr| grep 'VGA')"
 
-#Switch to laptop screen if VGA or DP is being used
-#Switch to VGA and DP if laptop is being used
-case "$screenmode" in
-	VGA1)
-		xrandr --output LVDS1 --auto --output DP1 --off --output VGA1 --off
-	;;
-	DP1)	
-		xrandr --output LVDS1 --auto --output DP1 --off --output VGA1 --off
-	;;
-	*)
-		xrandr --output LVDS1 --off --output DP1 --auto --output VGA1 --auto
-	;;
-esac
+#check if VGA is connected and being used, turn vga off and lvds on if true
+if echo "$screenmode" | grep -E 'connected [0-9]'; then
+	xrandr --output LVDS1 --auto --output DP1 --off --output VGA1 --off
+	exit 0
+fi
+#check if VGA is connected and is not being used, turn vga on and lvds off if true
+if echo "$screenmode" | grep -E ' connected'; then
+	xrandr --output LVDS1 --off --output DP1 --auto --output VGA1 --auto
+	exit 0
+fi
+#vga is disconnected so enable lvds
+xrandr --output LVDS1 --auto --output DP1 --off --output VGA1 --off
+exit 0
